96db101e66cfd338f79675ed190891037f168473
Sandi Sopian <shunasand73@gmail.com>
2022-08-23 21:04:41 +0700

    First Commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    &copy ;
+    <a
+      href='https://github.com/rajshekhar26/cleanfolio'
+      className='link footer__link'
+    >
+      Sandi Sopian 2022
+    </a>
+  </footer>
+)
+
+export default Footer
