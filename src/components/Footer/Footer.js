ae607209db97df2856b2a445f3034659cabee3b8
Sandi Sopian <shunasand73@gmail.com>
2022-09-16 21:02:34 +0700

    update blog link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,11 @@
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => (
-  <footer className='footer'>
-    &copy ;
-    <a
-      href='https://github.com/rajshekhar26/cleanfolio'
-      className='link footer__link'
-    >
-      Sandi Sopian 2022
+  <footer className="footer">
+    <a href="https://nakamapedia.blogspot.com/" className="link footer__link">
+      ©Sandi Sopian 2022
     </a>
   </footer>
-)
+);
 
-export default Footer
+export default Footer;
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,71 +1,90 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://github.com/SandiSopian/clean-portfolio',
-  title: 'SS.',
-}
+  homepage: "https://nakamapedia.blogspot.com/",
+  title: "SS.",
+};
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
-  description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  name: "Sandi Sopian",
+  role: "Front End Engineer",
+  description: "I'm work for developing, plan, design, build, and implement the user interface systems of websites, software programs, and web-based applications.",
+  resume: "https://drive.google.com/file/d/15khwRgkuRWXa2abYbGYBcJtIgqE24e2H/view?usp=sharing",
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: "https://www.linkedin.com/in/sandi-sopian-007623227/",
+    github: "https://github.com/SandiSopian",
   },
-}
+};
 
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    name: "Landing Page",
+    description: "I'm build simple Landing Pages for selling meats of cow and chickens",
+    stack: ["React", "Javascript", "CSS"],
+    sourceCode: "https://github.com/SandiSopian/Landing-Page_Dagangan",
+    livePreview: "https://jongko-daging-mang-umis.vercel.app/",
   },
   {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    name: "Blog",
+    description: "I'm build simple Blog for personal and commercial ",
+    stack: ["Next", "JavaScript", "SCSS", "CSS"],
+    sourceCode: "https://github.com/SandiSopian/personal-blog-v1",
+    livePreview: "https://personal-blog-v1.vercel.app/",
   },
   {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    name: "Rest API",
+    description: "I'm build simple Rest API for getting Pokemon information from PokeAPI",
+    stack: ["HTML", "JavaScript", "CSS"],
+    sourceCode: "https://github.com/SandiSopian/Pokemon-app",
+    livePreview: "https://pokemon-app-nine-umber.vercel.app/",
   },
-]
+  {
+    name: "Rest API",
+    description: "I'm build simple Rest API for getting Movie IMDb information from IMDb API",
+    stack: ["HTML", "JavaScript", "PHP"],
+    sourceCode: "https://github.com/SandiSopian/IMBD-web",
+    livePreview: "https://imbd-web.vercel.app/",
+  },
+  {
+    name: "Rest API",
+    description: "I'm build simple Rest API for getting Recipe information from Edamam API",
+    stack: ["HTML", "JavaScript", "CSS"],
+    sourceCode: "https://github.com/SandiSopian/Recipe-App",
+    livePreview: "https://simple-recipe-app-omega.vercel.app/",
+  },
+  {
+    name: "Landing Page",
+    description: "I'm slicing my own figma design for e-commerce plants shop",
+    stack: ["Next", "JavaScript", "TailwindCSS"],
+    sourceCode: "https://github.com/SandiSopian/Figma-to-NextJs-Tailwind---Slicing-1",
+    livePreview: "https://figma-to-next-js-tailwind-slicing-ver-1.vercel.app/",
+  },
+];
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
-]
+  "HTML",
+  "CSS",
+  "SASS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next",
+  "Bootstrap",
+  "TailwindCSS",
+  "Material UI",
+  "Git",
+  "Figma",
+  "Canva",
+  "Adobe",
+];
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'shunasand73@gmail.com',
-}
+  email: "shunasand73@gmail.com",
+};
 
-export { header, about, projects, skills, contact }
+export { header, about, projects, skills, contact };
